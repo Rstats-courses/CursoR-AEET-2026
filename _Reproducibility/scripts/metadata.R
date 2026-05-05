2d84b85349c53d0a8a01b51823ef3ca0a6f4a56f
Nacho Bartomeus <nacho.bartomeus@gmail.com>
2026-05-05 16:51:32 +0200

    Add metadata example

--- _Reproducibility/scripts/metadata.R
+++ _Reproducibility/scripts/metadata.R
@@ -7,19 +7,19 @@ devtools::install_github("ropenscilabs/dataspice")
 library(dataspice)
 
 #load data (see demo.qmd for details)
-d <- read.csv("slides/_Reproducibility/data/Herbivore_specialisation.csv", row.names = 1)
+d <- read.csv("_Reproducibility/data/Herbivore_specialisation.csv", row.names = 1)
 head(d)
 
 #create basic .csv metadata files and folders.
-create_spice(dir = "slides/_Reproducibility/data")
+create_spice(dir = "_Reproducibility/data")
 
 #Add creators
-edit_creators(metadata_dir = "slides/_Reproducibility/data/metadata")
+edit_creators(metadata_dir = "_Reproducibility/data/metadata")
 
 #Add how to access the data
-prep_access(data_path = "slides/_Reproducibility/data/",
-            access_path = "slides/_Reproducibility/data/metadata/access.csv")
-edit_access(metadata_dir = "slides/_Reproducibility/data/metadata")
+prep_access(data_path = "_Reproducibility/data/",
+            access_path = "_Reproducibility/data/metadata/access.csv")
+edit_access(metadata_dir = "_Reproducibility/data/metadata")
 
 #Add metadata
 
@@ -28,21 +28,22 @@ edit_access(metadata_dir = "slides/_Reproducibility/data/metadata")
 #range(d$Latitude, na.rm = T)
 #range(d$Longitude, na.rm = T)
 
-edit_biblio(metadata_dir = "slides/_Reproducibility/data/metadata")
+edit_biblio(metadata_dir = "_Reproducibility/data/metadata")
 
 #Describe variables
-prep_attributes(data_path = "slides/_Reproducibility/data/",
-                attributes_path = "slides/_Reproducibility/data/metadata/attributes.csv")
+prep_attributes(data_path = "_Reproducibility/data/",
+                attributes_path = "_Reproducibility/data/metadata/attributes.csv")
 colnames(d)
-edit_attributes(metadata_dir = "slides/_Reproducibility/data/metadata")
+edit_attributes(metadata_dir = "_Reproducibility/data/metadata")
 
 #create a json file
-write_spice(path = "slides/_Reproducibility/data/metadata")
+write_spice(path = "_Reproducibility/data/metadata")
 
 #look at the json:
-jsonlite::read_json(here::here("data", "metadata", "dataspice.json")) %>% listviewer::jsonedit()
+jsonlite::read_json("_Reproducibility/data/metadata/dataspice.json") %>% listviewer::jsonedit()
 
 #build a webpage!
-build_site()
+build_site(path = "_Reproducibility/data/metadata/dataspice.json",
+           out_path = "_Reproducibility/docs/index.html")
 
 
